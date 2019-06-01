a8e59fd15fee5ff01317109e69f0ff2a8a959d94
WS NAS NUVENS <36WSNASNUVENS@senacrjedu.local>
2019-06-01 10:27:26 -0300

    Fixed the first bug

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
 application = Flask(__name__)
-@application.route(‘/’)
+@application.route('/')
 
 def main():
-	return render_template(‘index.html’)
+    return render_template('index.html')
  
